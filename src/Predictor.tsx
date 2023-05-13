d0fee49af43ea55f020cbba4d3384d45af0bdbfb
magnus <magnus.wahlstrand@gmail.com>
2023-05-13 15:22:50 +0200

    predict from other image, working

--- src/Predictor.tsx
+++ src/Predictor.tsx
@@ -1,10 +1,26 @@
 import {MobileNetModel} from "./classes.ts";
 import Card2 from "./Card2.tsx";
+import React from "react";
 
 export function Predictor(props: { model: MobileNetModel | null }) {
+    const blueRef = React.useRef<HTMLImageElement>(null);
+
+    const handlePredict = () => {
+        console.log("Predicting...");
+        if(props.model === null || blueRef.current === null)
+            return;
+        props.model?.predict(blueRef);
+    }
+
     return <Card2 title={"Predict"} onUpdateTitle={(s) => ({})}>
         <h3 className="text-sm font-medium mb-3">
             aa {props.model === null ? null : "YEAH"}
         </h3>
+        <img className="w-12" ref={blueRef}
+             src="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAOAAAADgCAIAAACVT/22AAACaklEQVR4nO3SMQEAIAzAsIF/z+CAlx6Jgh5dM2egav8OgBeDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXNoKQZlDSDkmZQ0gxKmkFJMyhpBiXtAmGzAr9AoqBAAAAAAElFTkSuQmCC"/>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handlePredict}>
+            Predict
+        </button>
     </Card2>
 }
